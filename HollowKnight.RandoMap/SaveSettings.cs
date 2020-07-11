19e6bff93c3ec58bf98a11ff662dd9ee2027a902
The Great Gallus <thegreatgallus@gmail.com>
2020-07-11 00:58:39 -0400

    Create SaveSettings.cs

---
+++ HollowKnight.RandoMap/SaveSettings.cs
@@ -0,0 +1,26 @@
+﻿using Modding;
+using SeanprCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandoMapMod
+{
+	public class SaveSettings : BaseSettings
+	{
+		public static SaveSettings Instance;
+		public SaveSettings()
+		{
+			AfterDeserialize += () =>
+			{
+
+			};
+			Instance = this;
+		}
+
+		public bool MapsGiven
+		{
+			get => GetBool(false);
+			set => SetBool(value);
+		}
+	}
+}
